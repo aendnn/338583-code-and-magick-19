363dcb7f98dc691928501c27f1b6311e9645497d
Anna <aendnn9@gmail.com>
2020-01-29 20:17:34 +0200

    Добавляет правки

--- js/setup.js
+++ js/setup.js
@@ -2,64 +2,58 @@
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COAT_COLORS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var similarList = document.querySelector('.setup-similar-list');
 var template = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-var fragment = document.createDocumentFragment();
 
-// получение случайного числа
-var getRandomElement = function (min, max) {
+var wizards = [];
+
+// получение случайного индекса элемента
+var getRandomIndex = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
-var wizards = [
-  {
-    name: WIZARD_NAMES[getRandomElement(0, WIZARD_NAMES.length)],
-    surname: WIZARD_SURNAMES[getRandomElement(0, WIZARD_SURNAMES.length)],
-    coatColor: WIZARD_COAT_COLORS[getRandomElement(0, WIZARD_COAT_COLORS.length)],
-    eyesColor: WIZARD_EYES_COLORS[getRandomElement(0, WIZARD_EYES_COLORS.length)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomElement(0, WIZARD_NAMES.length)],
-    surname: WIZARD_SURNAMES[getRandomElement(0, WIZARD_SURNAMES.length)],
-    coatColor: WIZARD_COAT_COLORS[getRandomElement(0, WIZARD_COAT_COLORS.length)],
-    eyesColor: WIZARD_EYES_COLORS[getRandomElement(0, WIZARD_EYES_COLORS.length)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomElement(0, WIZARD_NAMES.length)],
-    surname: WIZARD_SURNAMES[getRandomElement(0, WIZARD_SURNAMES.length)],
-    coatColor: WIZARD_COAT_COLORS[getRandomElement(0, WIZARD_COAT_COLORS.length)],
-    eyesColor: WIZARD_EYES_COLORS[getRandomElement(0, WIZARD_EYES_COLORS.length)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomElement(0, WIZARD_NAMES.length)],
-    surname: WIZARD_SURNAMES[getRandomElement(0, WIZARD_SURNAMES.length)],
-    coatColor: WIZARD_COAT_COLORS[getRandomElement(0, WIZARD_COAT_COLORS.length)],
-    eyesColor: WIZARD_EYES_COLORS[getRandomElement(0, WIZARD_EYES_COLORS.length)]
+// добавляет объекты магов в массив
+var pushObjects = function (array) {
+  for (var i = 0; i < 4; i++) {
+    var wizardNew = {
+      name: WIZARD_NAMES[getRandomIndex(0, WIZARD_NAMES.length)],
+      coatColor: WIZARD_COAT_COLORS[getRandomIndex(0, WIZARD_COAT_COLORS.length)],
+      eyesColor: WIZARD_EYES_COLORS[getRandomIndex(0, WIZARD_EYES_COLORS.length)]
+    };
+    array.push(wizardNew);
   }
-];
-
-document.querySelector('.setup').classList.remove('hidden');
+  return array;
+};
 
 // отрисовка мага
 var renderWizard = function (wizard) {
   var wizardElement = template.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name + ' ' + wizard.surname;
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name + ' ' + WIZARD_SURNAMES[getRandomIndex(0, WIZARD_SURNAMES.length)];
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
 
   return wizardElement;
 };
 
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
+// складывает элементы во фрагмент для быстродействия
+var addWizards = function (array) {
+  var fragment = document.createDocumentFragment();
 
-similarList.appendChild(fragment);
+  for (var i = 0; i < array.length; i++) {
+    fragment.appendChild(renderWizard(array[i]));
+  }
 
+  similarList.appendChild(fragment);
+};
+
+pushObjects(wizards);
+addWizards(wizards);
+
+document.querySelector('.setup').classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');

17bb2ddd6bafa00681e68a25e59cdfb684376ca0
/\nna <aendnn9@gmail.com>
2020-02-01 15:38:44 +0200

    Учебный проект: нас орда (#3)
    * Добавляет третье задание
    
    * Добавляет правки
    
    * Добавляет правки
    
    * Добавляет правки
    
    * Исправляет замечания по функциям
    
    * Редактирует коммент функции

---
+++ js/setup.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var WIZARD_NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+
+var WIZARD_SURNAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+
+var WIZARD_COAT_COLORS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
+var WIZARD_EYES_COLORS = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
+var TOTAL_WIZARDS = 4;
+var wizards = [];
+
+var similarList = document.querySelector('.setup-similar-list');
+var template = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+// получение случайного индекса элемента
+var getRandomIndex = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+// возвращает случайный элемент массива
+var getRandomThing = function (array) {
+  return array[getRandomIndex(0, array.length)];
+};
+
+// добавляет магов в массив
+var getWizards = function (array, quantity) {
+  for (var i = 0; i < quantity; i++) {
+    var wizardNew = {
+      name: getRandomThing(WIZARD_NAMES) + ' ' + getRandomThing(WIZARD_SURNAMES),
+      coatColor: getRandomThing(WIZARD_COAT_COLORS),
+      eyesColor: getRandomThing(WIZARD_EYES_COLORS)
+    };
+    array.push(wizardNew);
+  }
+  return array;
+};
+
+// отрисовка мага
+var renderWizard = function (wizard) {
+  var wizardElement = template.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+
+  return wizardElement;
+};
+
+// складывает элементы во фрагмент для быстродействия
+var generateWizards = function (array) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < array.length; i++) {
+    fragment.appendChild(renderWizard(array[i]));
+  }
+
+  similarList.appendChild(fragment);
+};
+
+getWizards(wizards, TOTAL_WIZARDS);
+generateWizards(wizards);
+
+document.querySelector('.setup').classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
